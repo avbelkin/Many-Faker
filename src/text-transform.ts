3d22e0d6f75230860ccb91ef50079a3cfeedaa40
Alexey Belkin <alexey.belkin@mail.ru>
2023-07-16 02:27:15 +0800

    works nice, with dates and proper formatting of random

--- src/text-transform.ts
+++ src/text-transform.ts
@@ -1,4 +1,4 @@
-import { faker } from '@faker-js/faker'
+import { faker } from '@faker-js/faker/locale/en'
 import * as d3 from "d3"
 //const orPattern = /\{.*?(|.*?)+\}/g
 
@@ -14,22 +14,31 @@ function replacer(match: string, start: number): string {
     let format = parts[0][4]; // like .2f
 
     let [name, param1, param2] = getParams(parts[0][1]); //replacer name to match with
-    //debugger;
+    debugger;
     const replacer = Replacers.find(i => i.pattern === name);
     if(replacer == null) return match;
     
-    format = format ?? replacer.d3format
+    format = format ?? replacer.d3format ?? replacer.d3dateformat;
     
     if(!format)
         return replacer.fn();
 
-    if(!replacer.d3params)
+    if(!replacer.defaultValues)
         return d3.format(format)(replacer.fn());
 
-    param1 = param1 || (replacer.d3params && replacer.d3params[0]);
-    param2 = param2 || (replacer.d3params && replacer.d3params[1]);
+    param1 = param1 || (replacer.defaultValues && replacer.defaultValues[0]);
+    param2 = param2 || (replacer.defaultValues && replacer.defaultValues[1]);
 
-    return d3.format(format)(replacer.fn(param1, param2));
+
+    if(replacer.d3dateformat)
+    {
+         
+        return d3.utcFormat(format)(replacer.fn(param1));
+    }
+    else
+    {
+        return d3.format(format)(replacer.fn(param1, param2));
+    }
 }
 
 const scalePartRegex = /(.*?)((\d{1,10})([KMB])?(-(\d{1,10})([KMB])?)?)/g
@@ -39,18 +48,18 @@ function getParams(nameAndScale: string): [name: string, param1?: number, param2
     if(!scaleParams || !scaleParams[0] || !scaleParams[0][1]) return [nameAndScale];
 
     const name = scaleParams[0][1];
-    let param1: number | undefined;
-    let param2: number | undefined;
+    let startParam: number | undefined;
+    let endParam: number | undefined;
     if(scaleParams[0][3])
     {
-        param1 = convertScale(scaleParams[0][3], scaleParams[0][4])
+        startParam = convertScale(scaleParams[0][3], scaleParams[0][4])
     }
     if(scaleParams[0][6])
     {
-        param2 = convertScale(scaleParams[0][6], scaleParams[0][7])
+        endParam = convertScale(scaleParams[0][6], scaleParams[0][7])
     }
 
-    return [name, param1, param2]
+    return [name, endParam ? endParam : startParam, endParam ? startParam : endParam ]
 }
 
 function convertScale(numString: string, scaleString: string): number {
@@ -71,14 +80,39 @@ type Replacer =
     pattern: string
     fn: Function | ((param1?: number) => void) | ((param1?: number, param2?: number) => void)
     d3format?: string
-    d3params?: Array<number>;
+    d3dateformat?: string
+    defaultValues?: Array<number>;
 }
 
 const Replacers: Array<Replacer> = [
-    { pattern: "random", fn: (max: number, min: number)=> d3.randomUniform(min, max)().toString(), d3format: "d", d3params: [100, 0] },
-    { pattern: "randomN", fn: (center: number, deviation: number)=> d3.randomNormal(center, deviation ?? (center / 4) )().toString(), d3format: "d", d3params: [100] },
-    { pattern: "money", fn: (max: number, min: number)=> d3.randomUniform(min, max)().toString(), d3format: ",.2f", d3params: [100, 0] },
-    { pattern: "moneyN", fn: (center: number, deviation: number)=> d3.randomNormal(center, deviation ?? (center / 4) )().toString(), d3format: ",.2f", d3params: [10000] },
+    { pattern: "random", fn: (max: number, min: number)=> d3.randomUniform(min, max)().toString(), d3format: "d", defaultValues: [100, 0] },
+    { pattern: "money", fn: (max: number, min: number)=> d3.randomUniform(min, max)().toString(), d3format: ",.2f", defaultValues: [100, 0] },
+    { pattern: "randomN", fn: (max: number, min: number)=> {
+        const center = ((max - min) / 2) + min;
+        const deviation = (max - min) / 6;
+        const r = d3.randomNormal(center, deviation)();
+        const retval = Math.min(Math.max(min, r), max);
+        return retval.toString()
+    }, d3format: "d", defaultValues: [100, 0] },
+    { pattern: "moneyN", fn: (max: number, min: number)=> {
+        const center = ((max - min) / 2) + min;
+        const deviation = (max - min) / 6;
+        const r = d3.randomNormal(center, deviation)();
+        const retval = Math.min(Math.max(min, r), max);
+        return retval.toString()
+    }, d3format: ".2f", defaultValues: [100, 0] },
+    { pattern: "randomE", fn: (max: number, min: number)=>{
+        debugger;
+        let r = min+(d3.randomExponential(8)()*max);
+        const retval = Math.min(Math.max(min, r), max);
+        return retval.toString()}
+        , d3format: "d", defaultValues: [100, 0]},
+    { pattern: "moneyE", fn: (max: number, min: number)=>{
+        let r = min+(d3.randomExponential(8)()*max);
+        const retval = Math.min(Math.max(min, r), max);
+        return retval.toString()}
+        , d3format: ".2f", defaultValues: [100, 0]},
+
     //
     
     //
@@ -100,12 +134,27 @@ const Replacers: Array<Replacer> = [
     // company
     { pattern: "companyName", fn: ()=> faker.company.name() },
     { pattern: "companySuffix", fn: ()=> faker.company.companySuffix() },
-
-    //finance
-
     //special
     { pattern: "party", fn: ()=> faker.company.name() },
     { pattern: "client", fn: ()=> `${faker.company.name()} ${faker.location.countryCode}` },
     { pattern: "counterparty", fn: ()=> faker.company.name() },
+
+    // dates
+    { pattern: "recent", fn: (days: number)=> faker.date.recent(days), d3dateformat: "%d-%b-%Y", defaultValues: [7] },
+    { pattern: "recentDatetime", fn: (days: number)=> faker.date.recent(days), d3dateformat: "%d-%b-%Y %H:%M:%S", defaultValues: [7] },
+    { pattern: "soon", fn: (days: number)=> faker.date.soon(days), d3dateformat: "%d-%b-%Y", defaultValues: [7] },
+    { pattern: "soonDatetime", fn: (days: number)=> faker.date.soon(days), d3dateformat: "%d-%b-%Y %H:%M:%S", defaultValues: [7] },
+
+    //finance
+    { pattern: "masked", fn: ()=> faker.finance.maskedNumber({ length: 4, parens: false }) },
+    { pattern: "ccy", fn: ()=> faker.finance.currencyCode() },
+    { pattern: "currency", fn: ()=> faker.finance.currencyName() },
+    { pattern: "$", fn: ()=> faker.finance.currencySymbol() },
+    { pattern: "iban", fn: ()=> faker.finance.iban() },
+    { pattern: "bic", fn: ()=> faker.finance.bic({ includeBranchCode: true  }) },
+
+    //bank specific
+    { pattern: "crds", fn: (max: number, min: number)=> d3.randomUniform(min, max)().toString(), d3format: "d", defaultValues: [10000000, 20000000] },
+    { pattern: "aminet", fn: (max: number, min: number)=> `0${d3.randomUniform(min, max)()}`, d3format: "d", defaultValues: [900, 100] },
 ]
     
